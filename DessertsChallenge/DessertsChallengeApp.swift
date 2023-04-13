118f49bc746a44a3cd13fb2e60b520745a08bc6f
Ismail Elmaliki <check4elmaliki@live.com>
2023-04-13 12:46:40 -0400

    Initial Commit

---
+++ DessertsChallenge/DessertsChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertsChallengeApp.swift
+//  DessertsChallenge
+//
+//  Created by Ismail Elmaliki on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct DessertsChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
